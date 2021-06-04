adb7b81b040b9607248d2dd9ab931b5d8f6d5e32
alperkarasuer <>
2021-06-04 18:47:18 +0300

    Class Structures, still need to implement cell functions and game rules.

--- main.py
+++ main.py
@@ -1,70 +1,82 @@
 import pygame
 import numpy as np
 
-def gridUpdate(currentGrid):
-    for row in range(n):
-        for column in range(n):
-            cornersAndVertices = np.zeros((3, 3))
-            if (row == 0 and column == 0):
-                cornersAndVertices[0][0] = 1
-            elif (row == 0 and column < n):
-                cornersAndVertices[0][1] = 1
-            elif (row == 0 and column == n):
-                cornersAndVertices[0][2] = 1
+class Game:
+    # Initialize a N by N board
+    def __init__(self,gridSize):
+        # Empty grid array
+        self.grid = np.zeros((gridSize, gridSize), dtype = bool)
+        self.gridSize = gridSize
 
-            if (row != 0 and column == 0):
-                cornersAndVertices[1][0] = 1
-            elif (row != 0 and column == n):
-                cornersAndVertices[1][2] = 1
+    # Checks whether a give cell is on edge or corner
+    def checkEdges(self, cellPos):
+        cornersAndEdges = np.zeros((3, 3))
+        if (cellPos[0] == 0 and cellPos[1] == 0):
+            cornersAndEdges[0][0] = 1
+        elif (cellPos[0] == 0 and cellPos[1] < self.gridSize):
+            cornersAndEdges[0][1] = 1
+        elif (cellPos[0] == 0 and cellPos[1] == self.gridSize):
+            cornersAndEdges[0][2] = 1
 
-            if (row == n and column == 0):
-                cornersAndVertices[2][0] = 1
-            elif (row == n and column < n):
-                cornersAndVertices[2][1] = 1
-            elif (row == n and column == n):
-                cornersAndVertices[2][2] = 1
+        if ((cellPos[0] > 0 and cellPos[0] < self.gridSize) and cellPos[1] == 0):
+            cornersAndEdges[1][0] = 1
+        elif ((cellPos[0] > 0 and cellPos[0] < self.gridSize) and cellPos[1] == self.gridSize):
+            cornersAndEdges[1][2] = 1
+
+        if (cellPos[0] == self.gridSize and cellPos[1] == 0):
+            cornersAndEdges[2][0] = 1
+        elif (cellPos[0] == self.gridSize and cellPos[1] < self.gridSize):
+            cornersAndEdges[2][1] = 1
+        elif (cellPos[0] == self.gridSize and cellPos[1] == self.gridSize):
+            cornersAndEdges[2][2] = 1
+
+        print(cornersAndEdges)
+
+    # A class for each of the cells with alive or dead attribute?
+
+class Board:
+    def __init__(self, n):
+        # Color definitions
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
+        self.green = (0, 255, 0)
+
+        # N by N grid
+        self.n = n
+        self.borderCount = n+1
+
+        # Height and width of each cell
+        self.width = 20
+        self.height = 20
+
+        # Margins between cells
+        self.margin = 5
+
+        # Set the height and width of the screen
+        self.window_size = [self.n * self.width + self.borderCount * self.margin, self.n * self.width + self.borderCount * self.margin]
+        self.screen = pygame.display.set_mode(self.window_size)
 
 
-# Color definitions
-black = (0, 0, 0)
-white = (255, 255, 255)
-green = (0, 255, 0)
+gameBoard = Board(20)
 
-# n by n grid
-n = 20
-borderCount = n+1
-
-# Height and width of each cell
-width = 20
-height = 20
-
-# Margins between cells
-margin = 5
-
-# Empty grid array
-grid = np.zeros((n,n), dtype = bool)
+game = Game(gameBoard.n)
+game.checkEdges((0,0))
 
 # Initial Conditions
-grid[8][10] = 1
-grid[9][10] = 1
-grid[10][10] = 1
+game.grid[8][10] = 1
+game.grid[9][10] = 1
+game.grid[10][10] = 1
 
 
 
 # Initialize pygame
 pygame.init()
 
-# Set the height and width of the screen
-WINDOW_SIZE = [n*width+borderCount*margin, n*width+borderCount*margin]
-screen = pygame.display.set_mode(WINDOW_SIZE)
-
 # Set title of screen
-pygame.display.set_caption("Array Backed Grid")
+pygame.display.set_caption("Game of Life")
 
 # Loop until the user clicks the close button.
 running = True
-
-gridUpdate(None)
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
@@ -76,20 +88,20 @@ while running:
             running = False  # Flag that we are done so we exit this loop
 
     # Set the screen background
-    screen.fill(black)
+    gameBoard.screen.fill(gameBoard.black)
 
     # Draw the grid
-    for row in range(n):
-        for column in range(n):
-            color = white
-            if grid[row][column] == 1:
-                color = green
-            pygame.draw.rect(screen,
+    for row in range(gameBoard.n):
+        for column in range(gameBoard.n):
+            color = gameBoard.white
+            if game.grid[row][column] == 1:
+                color = gameBoard.green
+            pygame.draw.rect(gameBoard.screen,
                              color,
-                             [(margin + width) * column + margin,
-                              (margin + height) * row + margin,
-                              width,
-                              height])
+                             [(gameBoard.margin + gameBoard.width) * column + gameBoard.margin,
+                              (gameBoard.margin + gameBoard.height) * row + gameBoard.margin,
+                              gameBoard.width,
+                              gameBoard.height])
 
     # Limit to 60 frames per second
     clock.tick(60)
